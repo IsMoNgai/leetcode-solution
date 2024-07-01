ffb562bf36416a2385701e5937c6f88d147df1ec
IsMoNgai <mongaiwaiyin@gmail.com>
2024-07-01 16:58:30 -0700

    Create 1791.cpp

---
+++ easy/1791.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int findCenter(vector<vector<int>>& edges) {
+        map<int, int> g;
+        for(auto e : edges) {
+            for(int i : e) {
+                g[i]++;
+            }
+        }
+        for(auto i : g) {
+            if(i.second == edges.size()) return i.first;
+        }
+        return 0;
+    }
+};
