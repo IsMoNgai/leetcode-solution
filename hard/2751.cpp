043f10ccd957bb08d81c92736989d4bc8bbc0f11
IsMoNgai <mongaiwaiyin@gmail.com>
2024-07-20 23:48:07 -0700

    Update 2751.cpp

--- hard/2751.cpp
+++ hard/2751.cpp
@@ -1,46 +1,49 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <map>
+#include <stack>
+
+using namespace std;
+
 class Solution {
 public:
     vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
-        map<int, vector<int>> collision;
+        vector<int> order = positions;
+        sort(order.begin(), order.end());
+        map<int, int> m;
         for(int i = 0; i < positions.size(); i++) {
-            if(directions[i] == 'R') {
-                for(int j = 0; j < positions.size(); j++) {
-                    if(directions[j] == 'L') {
-                        if(positions[i] < positions[j]) collision[i].push_back(j); 
-                    }
-                }
+            m[positions[i]] = i;
+        }
+        stack<int> s;
+        for(int i : order) {
+            if(directions[m[i]] == 'R') {
+                s.push(i);
             } else {
-                for(int j = 0; j < positions.size(); j++) {
-                    if(directions[j] == 'R') {
-                        if(positions[i] > positions[j]) collision[i].push_back(j); 
+                while(!s.empty() && healths[m[i]] > 0) {
+                    //if left > right
+                    //if left < right
+                    //if left == right
+                    int stackItem = s.top();
+                    s.pop();
+                    if(healths[m[i]] > healths[m[stackItem]]) {
+                        healths[m[i]]--;
+                        healths[m[stackItem]] = 0;
+                    } else if(healths[m[i]] < healths[m[stackItem]]) {
+                        healths[m[i]] = 0;
+                        healths[m[stackItem]]--;
+                        s.push(stackItem);
+                    } else {
+                        healths[m[i]] = 0;
+                        healths[m[stackItem]] = 0;
                     }
                 }
             }
         }
-
-        for(auto i : collision) {
-            for(int j : i.second) {
-                cout << j << " ";
-                if(healths[i.first] > 0 && healths[j] > 0) {
-                    int temp = healths[i.first];
-                    healths[i.first] -= healths[j];
-                    healths[j] -= temp;                    
-                }
-                if(healths[j])
-            }
-            cout << endl;
+        vector<int> res;
+        for(int i : healths) {
+            if(i > 0) res.push_back(i);
         }
-        return healths;
+        return res;
     }
 };
-
-/*
-self R move:
-    opposite: L move 
-    self pos < L move pos
-    collision
-self L move:
-    oppo: R
-    self pos < 
-
-*/
